269d8cda51d0ab6451402b88aa993da82d21f961
ScriptSculptorJS <cmcunningham27@yahoo.com>
2025-05-01 10:31:41 -0700

    Added html for when there are no completed activities in the Completed tab.

--- src/scripts/completed-card.js
+++ src/scripts/completed-card.js
@@ -2,16 +2,29 @@ import { completedActivities } from '../data/completed.js';
 
 // Generates html for the completed activities
 export function renderCompletedActivities() {
-  let html = '';
-
-  completedActivities.forEach((activity) => {
-    html += `
-      <div class="completed-container">
-        <p class="completed-name">${activity.name}</p>
-        <p class="completed-date js-completed-date" date-completed-name="${activity.name}"><b>${activity.date}</b></p>
+  if (completedActivities.length === 0) {
+    document.querySelector('.js-activities-completed-card').innerHTML = `
+      <div class="empty-activities-message">
+        <h3>
+          Looks like you haven't completed an activity yet!
+        </h3>
+        <p>
+          Start by completing an activity from your Activities list or get your first activity suggestion above!
+        </p>
       </div>
     `
-  });
+  } else if (completedActivities !== 0) {
+    let html = '';
 
-  document.querySelector('.js-activities-completed-card').innerHTML = html;
+    completedActivities.forEach((activity) => {
+      html += `
+        <div class="completed-container">
+          <p class="completed-name">${activity.name}</p>
+          <p class="completed-date js-completed-date" date-completed-name="${activity.name}"><b>${activity.date}</b></p>
+        </div>
+      `
+    });
+
+    document.querySelector('.js-activities-completed-card').innerHTML = html;
+  }
 }
